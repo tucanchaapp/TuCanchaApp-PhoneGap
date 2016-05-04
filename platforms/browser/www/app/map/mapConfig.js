47cb5400cace45224ae49acdfa4f1bf6e6a90f57
sebasdeldi <sebasdeldi123@gmail.com>
2016-05-04 18:50:56 -0500

    cool ass loader implemented

--- platforms/browser/www/app/map/mapConfig.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApplicationModule', ['uiGmapgoogle-maps']).config(
-    ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
-        GoogleMapApiProviders.configure({
-            china: true
-        });
-    }]
-);
