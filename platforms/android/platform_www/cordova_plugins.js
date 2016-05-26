68d5c29ddadc25dc75c13edee5afdd7fdc03ba50
sebasdeldi <sebasdeldi123@gmail.com>
2016-05-26 12:12:13 -0500

    password recovery function implemented

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -3,12 +3,14 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
         "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-whitelist": "1.1.0"
+};
 // BOTTOM OF METADATA
 });
